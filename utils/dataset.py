297ee31eefe99b37512d2a2c68cdc3b5a28cf7e4
Sergei Porkhun <sergeiporkhun@yandex-team.ru>
2021-10-18 00:55:52 +0300

    Added creation dataset for Flicker8k

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,4 +1,7 @@
+import re
+
 from typing import Callable
+from os.path import join as join_path
 
 import cv2
 import torch
@@ -46,3 +49,60 @@ class TextAndImage(Dataset):
             'image': img,
             'text': tokenized_text
         }
+
+
+def create_dataset(
+        image_dir: str,
+        dir_to_train_file: str,
+        dir_to_valid_file: str,
+        dir_to_caption_file: str,
+        tokenizer: Callable,
+        max_size_seq_len: int,
+        transform: Callable
+):
+    train_images = set()
+    with open(dir_to_train_file) as f:
+        for line in f:
+            train_images.add(line.strip())
+    test_images = set()
+    with open(dir_to_valid_file) as f:
+        for line in f:
+            test_images.add(line.strip())
+    train_df = {
+        'text': [],
+        'image': []
+    }
+    test_df = {
+        'text': [],
+        'image': []
+    }
+    with open(dir_to_caption_file) as f:
+        for line in f:
+            img, description = re.findall(r'^([\w]+.jpg)#[0-9]+\t(.+.)$', line.strip())
+            img_path = join_path(image_dir, img)
+            if img_path in train_images:
+                if img_path in train_df:
+                    train_df[img_path].append(description)
+                else:
+                    train_df[img_path] = [description]
+            else:
+                if img_path in test_df:
+                    test_df[img_path].append(description)
+                else:
+                    test_df[img_path] = [description]
+    train_csv = pd.DataFrame(train_df)
+    test_csv = pd.DataFrame(test_df)
+    return (
+        TextAndImage(
+            csv=train_csv,
+            tokenizer=tokenizer,
+            max_size_seq_len=max_size_seq_len,
+            transform=transform['train']
+        ),
+        TextAndImage(
+            csv=test_csv,
+            tokenizer=tokenizer,
+            max_size_seq_len=max_size_seq_len,
+            transform=transform['test']
+        )
+    )
