6240c4ff8b6b3f44deab9addf0c0cf3df3304bf0
Sergei Porkhun <sergeiporkhun@yandex-team.ru>
2021-10-19 01:43:49 +0300

    Refactor code for new dataset flickr30k from Kaggle

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,6 +1,5 @@
 import re
 
-from random import randint
 from typing import Callable
 from os.path import join as join_path
 
@@ -8,6 +7,7 @@ import cv2
 import torch
 import pandas as pd
 
+from numpy.random import shuffle, choice
 from torch.utils.data import Dataset
 
 
@@ -27,15 +27,17 @@ class TextAndImage(Dataset):
 
     def __getitem__(self, item):
         img = cv2.cvtColor(
-            cv2.imread(self.csv['image'][item]),
+            cv2.imread(self.csv['image_name'][item]),
             cv2.COLOR_BGR2RGB
         )
         if self.transform is not None:
             img = self.transform(image=img)['image']
 
-        texts = self.csv['text'][item]
-        lenght_of_texts = len(texts)
-        text = texts[randint(0, lenght_of_texts - 1)]
+        texts = self.csv['comment'][item]
+        marks = [5 - x for x in self.csv['comment_number'][item]]
+        probability = [x / sum(marks) for x in marks]
+
+        text = choice(texts, p=probability)
         tokenized_text = self.tokenizer(
             text,
             return_tensors="pt"
@@ -58,62 +60,34 @@ class TextAndImage(Dataset):
 
 
 def create_dataset(
-        image_dir: str,
-        dir_to_train_file: str,
-        dir_to_valid_file: str,
-        dir_to_caption_file: str,
+        path_to_csv: str,
+        dir_image: str,
         tokenizer: Callable,
         max_size_seq_len: int,
         transform: Callable
 ):
-    train_images = set()
-    with open(dir_to_train_file) as f:
-        for line in f:
-            train_images.add(line.strip())
-    valid_images = set()
-    with open(dir_to_valid_file) as f:
-        for line in f:
-            valid_images.add(line.strip())
-    train_df = {
-        'text': [],
-        'image': []
-    }
-    valid_df = {
-        'text': [],
-        'image': []
-    }
-    with open(dir_to_caption_file) as f:
-        for line in f:
-            try:
-                img, description = re.findall(r'^([\w]+.jpg)#[0-9]+\t(.+.)$', line.strip())[0]
-            except IndexError:
-                continue
-            img_path = join_path(image_dir, img)
-            if img in train_images:
-                if img_path in train_df['image']:
-                    index = train_df['image'].index(img_path)
-                    train_df['text'][index].append(description)
-                else:
-                    train_df['image'].append(img_path)
-                    train_df['text'].append([description])
-            elif img in valid_images:
-                if img_path in valid_df:
-                    index = valid_df['image'].index(img_path)
-                    valid_df['text'][index].append(description)
-                else:
-                    valid_df['image'].append(img_path)
-                    valid_df['text'].append([description])
-    train_csv = pd.DataFrame(train_df)
-    valid_csv = pd.DataFrame(valid_df)
+    df = pd.read_csv(path_to_csv, delimiter='|')
+    df.columns = [x.strip() for x in df.columns]
+    for column in df.columns:
+        df[column] = df[column].apply(lambda x: x.strip() if isinstance(x, str) else x)
+    index = df['comment_number'].str.isdigit()
+    df = df[index]
+    df['image_name'] = df['image_name'].apply(lambda x: join_path(dir_image, x))
+    df['comment_number'] = pd.to_numeric(df['comment_number'])
+    shuffled_index = shuffle(df.index)
+    count_examples = len(shuffled_index)
+    train_index = shuffled_index[:round(count_examples * 0.8)]
+    valid_index = shuffled_index[round(count_examples * 0.8):]
+    train_df, valid_df = df[train_index], df[valid_index]
     return (
         TextAndImage(
-            csv=train_csv,
+            csv=train_df,
             tokenizer=tokenizer,
             max_size_seq_len=max_size_seq_len,
             transform=transform['train']
         ),
         TextAndImage(
-            csv=valid_csv,
+            csv=valid_df,
             tokenizer=tokenizer,
             max_size_seq_len=max_size_seq_len,
             transform=transform['valid']
