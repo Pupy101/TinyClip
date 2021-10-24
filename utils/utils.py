13bd86e7f8e79d62d1333f79814f0f6a8732ae82
Sergei Porkhun <sergeiporkhun@yandex-team.ru>
2021-10-24 22:13:29 +0300

    Update function 'freeze_weights' in 'utils.py'

--- utils/utils.py
+++ utils/utils.py
@@ -1,8 +1,12 @@
 from torch import nn
 
 
-def freeze_weights(model: nn.Module, last_index_freeze: int):
-    for weight in list(model.parameters())[:last_index_freeze]:
+def freeze_weights(model: nn.Module, last_index_freeze: int, freeze_all_net: bool = False):
+    if freeze_all_net:
+        weights = model.parameters()
+    else:
+        weights = list(model.parameters())[:last_index_freeze]
+    for weight in weights:
         weight.requires_grad = False
 
 
