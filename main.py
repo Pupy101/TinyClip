56719baab7e3c8238673b71b890d11f03dc3a261
Sergei Porkhun <sergeiporkhun@yandex-team.ru>
2021-11-03 19:12:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from config import Config as clip_config
 
 if __name__ == '__main__':
     if clip_config.TYPE_USING == 'train':
+        print('Training model')
         train_clip(clip_config)
     elif clip_config.TYPE_USING == 'eval':
+        print('Evaluation model')
         inference(clip_config)
