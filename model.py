6ff043c9343e33fad996d03b18b861210f450ec9
Sergei Porkhun <sergeiporkhun@yandex-team.ru>
2021-11-03 20:47:31 +0300

    Update model

--- model.py
+++ model.py
@@ -1,37 +1,11 @@
 from typing import Tuple
 
 import torch
+import numpy as np
 
 from torch import nn
 from torchvision import models
 from transformers import DistilBertForSequenceClassification
-
-
-class CosineSimilarity2DVectors(nn.Module):
-
-    def __init__(
-            self,
-            eps: float = 1e-6
-    ):
-        super().__init__()
-        self.eps = eps
-
-    def forward(
-            self,
-            vectors: Tuple[torch.Tensor],
-            normalize: bool = False
-    ) -> torch.Tensor:
-        first_vector, second_vector = vectors
-        assert len(first_vector.shape) == len(second_vector.shape) == 2, 'Vectors dimesions must be 2'
-        first_l2_norm = torch.linalg.norm(first_vector, dim=1).unsqueeze(1)
-        second_l2_norm = torch.linalg.norm(second_vector, dim=1).unsqueeze(0)
-        multiply_of_vectors = torch.sum(
-            first_vector.unsqueeze(2) * second_vector.permute(1, 0).unsqueeze(0),
-            dim=1
-        )
-        if normalize:
-            return multiply_of_vectors / (torch.sqrt(first_l2_norm * second_l2_norm) + self.eps)
-        return multiply_of_vectors
 
 
 class CLIP(nn.Module):
@@ -52,41 +26,73 @@ class CLIP(nn.Module):
         self.model_text_emb, output_dim_text = configuration_text_model(name_text_embedding)
         if overall_dim is None:
             overall_dim = max(output_dim_img, output_dim_text)
-        self.clf_img = nn.Sequential(
+        self.matrix_normalize_img_emb = nn.Sequential(
             nn.LeakyReLU(),
             nn.Linear(in_features=output_dim_img, out_features=overall_dim)
         )
-        self.clf_text = nn.Sequential(
+        self.matrix_normalize_text_emb = nn.Sequential(
             nn.LeakyReLU(),
             nn.Linear(in_features=output_dim_text, out_features=overall_dim)
         )
+        self.logit_scale = nn.Parameter(torch.ones([]) * np.log(1 / 0.07))
 
-        self.cosine_similarity = CosineSimilarity2DVectors()
+    def forward_img_net(self, img: torch.Tensor) -> torch.Tensor:
+            img_embedding = self.model_img_emb(img)
+            normalized_img_emb = self.matrix_normalize_img_emb(img_embedding)
+            image_features = normalized_img_emb / normalized_img_emb.norm(dim=-1, keepdim=True)
+            return image_features
+
+    def forward_txt_net(self, txt: torch.Tensor) -> torch.Tensor:
+            text_embedding = self.model_text_emb(txt)
+            normalized_text_emb = self.matrix_normalize_text_emb(text_embedding)
+            text_features = normalized_text_emb / normalized_text_emb.norm(dim=-1, keepdim=True)
+            return text_features
+
+    def forward_cosine_similarity(
+            self,
+            image_features: torch.Tensor,
+            text_features: torch.Tensor
+        ) -> Tuple[torch.Tensor]:
+        logit_scale = self.logit_scale.exp()
+        logits_image = logit_scale * image_features @ text_features.t()
+        logits_text = logits_image.t()
+        return logits_image, logits_text
 
     def forward(
             self,
             vectors: Tuple[torch.Tensor],
-    ) -> torch.Tensor:
+            image_features: torch.Tensor = None,
+            text_features: torch.Tensor = None,
+            is_raw_output: bool = False
+    ) -> Tuple[torch.Tensor]:
         img, text = vectors
-        img_emb = self.model_img_emb(img)
-        text_emb = self.model_text_emb(text)
-        classes_img = self.clf_img(img_emb)
-        classes_text = self.clf_text(text_emb)
-        output = self.cosine_similarity((classes_img, classes_text))
-        return output
 
+        if image_features is None:
+            image_features = self.forward_img_net(img)
+
+        if image_features is None:
+            text_features = self.forward_txt_net(text)
+        
+        logits_image, logits_text = self.forward_cosine_similarity(image_features, text_features)
+
+        if is_raw_output:
+            logits_image, logits_text, image_features, text_features
+
+        return logits_image, logits_text
+
+    @torch.no_grad()
     def inference(
             self,
             input_tensor: Tuple[torch.Tensor],
             is_rewrite_classes: bool = False
     ) -> torch.Tensor:
         img, text_classes = input_tensor
-        if hasattr(self, 'classes') and not is_rewrite_classes:
-            classes_text = self.classes
+        if hasattr(self, 'inference_text_embedding') and not is_rewrite_classes:
+            inference_text_embedding = self.inference_text_embedding
+            logits_image, logits_text = self.forward(img, text_classes, text_features=inference_text_embedding)
         else:
-            text_emb = self.model_text_emb(text_classes)
-            classes_text = self.clf_text(text_emb)
-            self.classes = classes_text
+            logits_image, logits_text, image_features, text_features = self.forward(img, text_classes, is_raw_output=True)
+            self.inference_text_embedding = text_features
         img_emb = self.model_img_emb(img)
         classes_img = self.clf_img(img_emb)
         output = self.cosine_similarity((classes_img, classes_text), normalize=True)
