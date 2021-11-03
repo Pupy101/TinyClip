4a1a6fb929f991f4566c6aaa02bf4b668f97cd2f
Sergei Porkhun <sergeiporkhun@yandex-team.ru>
2021-11-03 18:52:03 +0300

    Update main.py and add evaluation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from train import train_clip
+from inference import inference
 from config import Config as clip_config
 
 
@@ -6,4 +7,4 @@ if __name__ == '__main__':
     if clip_config.TYPE_USING == 'train':
         train_clip(clip_config)
     elif clip_config.TYPE_USING == 'eval':
-        pass
+        inference(clip_config)
