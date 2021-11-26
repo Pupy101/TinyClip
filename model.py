10c52859c0a2f4db96e994ec2911f2a81340d5f2
Sergei Porkhun <sergeiporkhun@yandex-team.ru>
2021-11-26 16:54:56 +0300

    Update model.py and add bert as text embedding model

--- model.py
+++ model.py
@@ -1,14 +1,21 @@
-from typing import Tuple
+from typing import Tuple, Union
 
 import torch
-import numpy as np
 
 from torch import nn
 from torchvision import models
-from transformers import DistilBertForSequenceClassification
+from transformers import (
+    DistilBertForSequenceClassification,
+    BertForSequenceClassification
+)
 
 
 class CLIP(nn.Module):
+    """
+    CLIP model with 2 parts:
+        image part - CNN
+        text part - BERT
+    """
 
     def __init__(
             self,
@@ -19,39 +26,40 @@ class CLIP(nn.Module):
         """
         name_image_embedding - name model for embedding image
         name_text_embedding - name model for embedding text
-        overall_dim - overall dimesion
+        overall_dim - overall dimension
         """
         super().__init__()
-        self.model_img_emb, output_dim_img = configuration_image_model(name_image_embedding)
-        self.model_text_emb, output_dim_text = configuration_text_model(name_text_embedding)
+        self.__inference_text_embedding = None
+        self.model_image_embedding, output_dim_img = configuration_image_model(name_image_embedding)
+        self.model_text_embedding, output_dim_text = configuration_text_model(name_text_embedding)
         if overall_dim is None:
             overall_dim = max(output_dim_img, output_dim_text)
-        self.matrix_normalize_img_emb = nn.Sequential(
+        self.linear_image = nn.Sequential(
             nn.LeakyReLU(),
             nn.Linear(in_features=output_dim_img, out_features=overall_dim)
         )
-        self.matrix_normalize_text_emb = nn.Sequential(
+        self.linear_text = nn.Sequential(
             nn.LeakyReLU(),
             nn.Linear(in_features=output_dim_text, out_features=overall_dim)
         )
 
-    def forward_img_net(self, img: torch.Tensor) -> torch.Tensor:
-            img_embedding = self.model_img_emb(img)
-            normalized_img_emb = self.matrix_normalize_img_emb(img_embedding)
-            image_features = normalized_img_emb / normalized_img_emb.norm(dim=-1, keepdim=True)
-            return image_features
+    def forward_img_part(self, img: torch.Tensor) -> torch.Tensor:
+        image_embedding = self.model_image_embedding(img)
+        normalized_img_emb = self.linear_image(image_embedding)
+        image_features = normalized_img_emb / normalized_img_emb.norm(dim=-1, keepdim=True)
+        return image_features
 
     def forward_txt_net(self, txt: torch.Tensor) -> torch.Tensor:
-            text_embedding = self.model_text_emb(txt)
-            normalized_text_emb = self.matrix_normalize_text_emb(text_embedding)
-            text_features = normalized_text_emb / normalized_text_emb.norm(dim=-1, keepdim=True)
-            return text_features
+        text_embedding = self.model_text_embedding(txt)
+        normalized_text_emb = self.linear_text(text_embedding)
+        text_features = normalized_text_emb / normalized_text_emb.norm(dim=-1, keepdim=True)
+        return text_features
 
+    @staticmethod
     def forward_cosine_similarity(
-            self,
             image_features: torch.Tensor,
             text_features: torch.Tensor
-        ) -> Tuple[torch.Tensor]:
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
         logits_image = image_features @ text_features.t()
         logits_text = logits_image.t()
         return logits_image, logits_text
@@ -62,35 +70,44 @@ class CLIP(nn.Module):
             image_features: torch.Tensor = None,
             text_features: torch.Tensor = None,
             is_raw_output: bool = False
-    ) -> Tuple[torch.Tensor]:
+    ) -> Union[
+        Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor],
+        Tuple[torch.Tensor, torch.Tensor]
+    ]:
         img, text = vectors
 
         if image_features is None:
-            image_features = self.forward_img_net(img)
+            image_features = self.forward_img_part(img)
 
         if text_features is None:
             text_features = self.forward_txt_net(text)
-        
-        logits_image, logits_text = self.forward_cosine_similarity(image_features, text_features)
+
+        (
+            logits_image,
+            logits_text
+        ) = CLIP.forward_cosine_similarity(
+            image_features,
+            text_features
+        )
 
         if is_raw_output:
-            logits_image, logits_text, image_features, text_features
+            return logits_image, logits_text, image_features, text_features
 
         return logits_image, logits_text
 
     @torch.no_grad()
     def inference(
             self,
-            input_tensor: Tuple[torch.Tensor],
+            input_tensor: Tuple[torch.Tensor, torch.Tensor],
             is_rewrite_classes: bool = False
     ) -> torch.Tensor:
-        img, text_classes = input_tensor
-        if hasattr(self, 'inference_text_embedding') and not is_rewrite_classes:
-            inference_text_embedding = self.inference_text_embedding
-            logits_image, _ = self.forward(img, text_classes, text_features=inference_text_embedding)
+        image, text = input_tensor
+        if self.__inference_text_embedding is not None and not is_rewrite_classes:
+            inference_text_embedding = self.__inference_text_embedding
+            logits_image, _ = self.forward(image, text, text_features=inference_text_embedding)
         else:
-            logits_image, *_, text_features = self.forward(img, text_classes, is_raw_output=True)
-            self.inference_text_embedding = text_features
+            logits_image, *_, text_features = self.forward(image, text, is_raw_output=True)
+            self.__inference_text_embedding = text_features
         return torch.argmax(logits_image, dim=1)
 
     @property
@@ -110,7 +127,9 @@ def configuration_image_model(name_model: str) -> Tuple[nn.Module, int]:
     elif name_model == 'resnext50':
         model = models.resnext50_32x4d(pretrained=True)
         model.fc = nn.Identity()
-        img_dim_size = 2048        
+        img_dim_size = 2048
+    else:
+        raise ValueError('Please type right model name')
     return model, img_dim_size
 
 
@@ -119,7 +138,6 @@ class WrapperModelFromHuggingFace(nn.Module):
     def __init__(self, hugging_face_model: nn.Module):
         super().__init__()
         self.model = hugging_face_model
-        
 
     def forward(self, x) -> torch.Tensor:
         return self.model(x)['logits']
@@ -127,8 +145,14 @@ class WrapperModelFromHuggingFace(nn.Module):
 
 def configuration_text_model(name_model: str) -> Tuple[nn.Module, int]:
     if name_model == 'distilbert':
-        bert = DistilBertForSequenceClassification.from_pretrained('distilbert-base-uncased')
-        bert.classifier = nn.Identity()
-        model = WrapperModelFromHuggingFace(bert)
-        text_dim_size = 768    
+        bert = DistilBertForSequenceClassification.from_pretrained(
+            'distilbert-base-uncased'
+        )
+    elif name_model == 'bert':
+        bert = BertForSequenceClassification.from_pretrained(
+            'bert-base-uncased'
+        )
+    bert.classifier = nn.Identity()
+    model = WrapperModelFromHuggingFace(bert)
+    text_dim_size = 768
     return model, text_dim_size
