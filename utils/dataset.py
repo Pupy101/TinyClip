0f22a97f3c4c537d483f983635349e23aef972a9
Pupy101 <ser.porkhun41@gmail.com>
2021-11-26 17:02:37 +0300

    Add docstring and typing to function creating dataset and dataset

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,7 +1,7 @@
 import os
 import json
 
-from typing import Callable
+from typing import Callable, Tuple, Dict
 from os.path import join as join_path
 
 import cv2
@@ -13,6 +13,22 @@ from torch.utils.data import Dataset
 
 
 class TextAndImageFromCSV(Dataset):
+    """
+    Torch dataset for images and texts
+    ----------------------------------
+    Input parameters:
+        csv - pandas.DataFrame with 3 columns -
+            1. path to image column ['image_name']
+            2. comment (text description) column ['comment']
+            3. comment rank (from 1 to 5) where 1 is best and 5 is worst description
+                it samples with np.random.choice
+        tokenizer - tokenizer for language model
+        max_size_seq_len - max size of token for text description
+        transform - transforms to image
+
+    Output parameters:
+        Dict with keys ['image', 'text']
+    """
 
     def __init__(
             self,
@@ -26,7 +42,7 @@ class TextAndImageFromCSV(Dataset):
         self.max_size_seq_len = max_size_seq_len
         self.transform = transform
 
-    def __getitem__(self, item):
+    def __getitem__(self, item) -> Dict[str, torch.Tensor]:
         img = cv2.cvtColor(
             cv2.imread(self.csv['image_name'].iat[item]),
             cv2.COLOR_BGR2RGB
@@ -55,7 +71,7 @@ class TextAndImageFromCSV(Dataset):
             'text': tokenized_text
         }
 
-    def __len__(self):
+    def __len__(self) -> int:
         return self.csv.shape[0]
 
 
@@ -66,7 +82,20 @@ def create_datasets_from_csv(
         max_size_seq_len: int,
         transform: Callable,
         df: pd.DataFrame = None
-):
+) -> Tuple[TextAndImageFromCSV, TextAndImageFromCSV]:
+    """
+    Function for creating train and valid datasets from csv
+    ----------------------------------
+    Input parameters:
+        path_to_csv - path to csv file used only if parameter df is None
+        dir_image - path to directory with images
+        tokenizer - tokenizer for language model
+        max_size_seq_len - max size of token for text description
+        transform - transforms to image
+        df - csv table with needed columns
+    Output parameters:
+        Tuple with two TextAndImageFromCSV (train and valid)
+    """
     if df is None:
         df = pd.read_csv(path_to_csv, delimiter='|')
     df.columns = [x.strip() for x in df.columns]
@@ -105,7 +134,20 @@ def create_datasets_from_json(
     tokenizer: Callable,
     max_size_seq_len: int,
     transform: Callable
-):
+) -> Tuple[TextAndImageFromCSV, TextAndImageFromCSV]:
+    """
+    Function for creating train and valid datasets from json
+    ----------------------------------
+    Input parameters:
+        json with data (path to images, texts and their rank)
+        dir_image - path to directory with images
+        tokenizer - tokenizer for language model
+        max_size_seq_len - max size of token for text description
+        transform - transforms to image
+        df - csv table with needed columns
+    Output parameters:
+        Tuple with two TextAndImageFromCSV (train and valid)
+    """
     df = {'image_name': [], 'comment': [], 'comment_number': []}
     df_file_and_sentence = set()
     for json_name in jsons:
