353a885e8a2d02114ade307af102de0bf3563095
Pupy101 <ser.porkhun41@gmail.com>
2022-01-01 18:17:29 +0300

    Update config format

--- config.py
+++ config.py
@@ -1,37 +1,18 @@
-from typing import Any, Dict, Union, Callable
-from collections import OrderedDict
+from typing import Any, Dict, Union
 
 from torch import optim, nn
-from transformers import (
-    DistilBertTokenizer,
-    BertTokenizer
-)
-
-from utils import augmentations
+from transformers import BertTokenizer
 
 
 class Config:
     TYPE_USING: str = 'train'  # or 'eval'
 
-    # for training i use
-    # https://www.kaggle.com/mrviswamitrakaushik/image-captioning-data
-    # otherwise you can use csv if change 'json' to 'csv'
-    DATASET_PARAMS: Dict[str, Union[str, int, Callable]] = {
-        'jsons': [
-            '/content/caption_datasets/dataset_coco.json',
-            '/content/caption_datasets/dataset_flickr30k.json',
-            '/content/caption_datasets/dataset_flickr8k.json'
-        ],
-        'dir_image': '/content/train2014/train2014',
-        'tokenizer': BertTokenizer.from_pretrained(
-            'bert-base-uncased'
-        ),
-        'max_size_seq_len': 30,
-        'transforms': {
-            'train': augmentations.train,
-            'valid': augmentations.valid
-        }
+    DATASETS_CSV = {
+        'train': '/content/train.csv',
+        'valid': '/content/valid.csv'
     }
+    TOKENIZER = BertTokenizer.from_pretrained('bert-base-uncased')
+    MAX_SEQUENCE_LEN = 20
 
     LOADER_PARAMS: Dict[str, Dict[str, Union[bool, int]]] = {
         'train': {
@@ -44,7 +25,7 @@ class Config:
         }
     }
 
-    MODEL_IMAGE_NAME: str = 'wide_resnet50'
+    MODEL_IMAGE_NAME: str = 'wide_resnet50_2'
     MODEL_IMAGE_PARAMETERS: Dict[str, Any] = {
         'pretrained': True
     }
@@ -55,10 +36,14 @@ class Config:
         'name_pretrained': 'bert-base-uncased'
     }
 
-    PATH_TO_WEIGHTS: Union[str, None] = None
+    PATH_TO_WEIGHTS: Dict[str, Union[str, None]] = {
+        'PRETRAINED_WEIGHTS': None,
+        'PATH_TO_SAVE': './train/weights'
+    }
 
     NUM_EPOCH: int = 30
     ACCUMULATE: bool = True
+    ACCUMULATION_STEPS: int = 2
 
     OPTIMIZER: nn.Module = optim.AdamW
     OPTIMIZER_PARAMS: dict = {
@@ -72,15 +57,8 @@ class Config:
         'anneal_strategy': 'cos'
     }
 
-    PATH_TO_SAVE_MODEL_WEIGHTS: str = './train_result'
-
     INFERENCE_PARAMS: Dict[str, Any] = {
         'TARGET_DIR': '/content/CLIP/test',
         'IMAGES_DIR': '/content/CLIP/predict',
-        'CLASSES': [
-            'Dog',
-            'Cat',
-            'Human',
-            'Car'
-        ]
+        'CLASSES': ['Dog', 'Cat', 'Human', 'Car']
     }
