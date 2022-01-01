767319ed76c0174a28463b784809fd34a2926ce8
Pupy101 <ser.porkhun41@gmail.com>
2022-01-01 18:18:17 +0300

    Change main.py with new configurator

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from src import train_clip, inference_clip
+from src import Configurator, inference_clip, train_clip
+
 from config import Config as clip_config
 
 
 if __name__ == '__main__':
+    configuration = Configurator(clip_config)
     if clip_config.TYPE_USING == 'train':
         print('Training model')
-        train_clip(clip_config)
+        train_clip(configuration)
     elif clip_config.TYPE_USING == 'eval':
         print('Evaluation model')
-        inference_clip(clip_config)
+        inference_clip(configuration)
