7e4814b50049ad8648188a4437d70c8933b6fb4c
Pupy101 <ser.porkhun41@gmail.com>
2022-01-02 00:34:25 +0300

    Create function for create labels from index of image

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,20 @@
+import torch
+
 from torch import nn
 
 
 def freeze_weight(model: nn.Module):
+    """
+    Function for freeze weight in model
+    :param model: torch model
+    """
     for weight in model.parameters():
         weight.requires_grad = False
+
+
+def create_label(index_of_pairs: torch.Tensor) -> torch.Tensor:
+    size = len(index_of_pairs)
+    label = torch.zeros((size, size))
+    for i in range(size):
+        label[i, :] = index_of_pairs == index_of_pairs[i]
+    return label
