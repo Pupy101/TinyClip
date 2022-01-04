2846e43fe5260f9299ca701d9532e40a08264036
Pupy101 <ser.porkhun41@gmail.com>
2022-01-04 17:50:48 +0300

    Add focal loss

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,6 @@
 from .augmentations import augmentations
 from .dataset import TextAndImageFromCSV, ImageFromCSV
+from .losses import FocalLoss
 from .utils import (
     freeze_weight,
     create_label_from_index,
