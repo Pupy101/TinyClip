375a9bd6b81b3693d58176dc87e83b8316b39e34
Pupy101 <ser.porkhun41@gmail.com>
2022-01-07 02:56:54 +0300

    Added error if type of using in config is wrong

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from src import Configurator, inference_clip, train_clip
 
-from config import Config as clip_config
+from config import Config
 
 
 if __name__ == '__main__':
-    configuration = Configurator(clip_config)
-    if clip_config.TYPE_USING == 'train':
-        print('Training model')
+    configuration = Configurator(Config)
+    if Config.TYPE_USING == 'train':
+        print('Training model:')
         train_clip(configuration)
-    elif clip_config.TYPE_USING == 'eval':
-        print('Evaluation model')
+    elif Config.TYPE_USING == 'eval':
+        print('Evaluation model:')
         inference_clip(configuration)
+    else:
+        raise ValueError('Strange type of using CLIP')
