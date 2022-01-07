787e50fc56feb9e9f8bf3cbb78c7623d7409a8eb
Pupy101 <ser.porkhun41@gmail.com>
2022-01-07 06:00:47 +0300

    Delete function for create labels from image name

--- utils/utils.py
+++ utils/utils.py
@@ -3,24 +3,26 @@ import torch
 from torch import nn
 
 
-def freeze_weight(model: nn.Module):
+def freeze_weight(model: nn.Module) -> None:
     """
     Function for freeze weight in model
+
     :param model: torch model
     """
     for weight in model.parameters():
         weight.requires_grad = False
 
 
-def create_label_from_index(index_of_pairs: torch.Tensor) -> torch.Tensor:
-    size = len(index_of_pairs)
-    label = torch.zeros((size, size))
-    for i in range(size):
-        label[i, :] = index_of_pairs == index_of_pairs[i]
-    return label
+def create_label_from_text_embedding(
+        text_embedding: torch.Tensor
+) -> torch.Tensor:
+    """
+    Function for creating labels from text embedding
 
-
-def create_label_from_text(text_vectors: torch.Tensor) -> torch.Tensor:
-    text_vectors = text_vectors.clone().detach()  # batch size x vector dim
-    similarity = text_vectors @ text_vectors.t()
+    :param text_embedding: normalized vector representation of text
+    from text model
+    :return: labels similarity of texts embedding
+    """
+    text_embedding = text_embedding.clone().detach()
+    similarity = text_embedding @ text_embedding.t()
     return (similarity > 0.5).float()
