912edee3247a6461e12fbc0f7ddef75602381e4f
Pupy101 <ser.porkhun41@gmail.com>
2022-05-02 22:10:03 +0300

    Refactor preprocessing dataset

--- scripts/preprocessing_dataset.py
+++ scripts/preprocessing_dataset.py
@@ -1,13 +1,12 @@
-import os
-import json
 import argparse
+import json
+import re
 
-from typing import Dict
-from os.path import join as path_join
+from copy import deepcopy
+from pathlib import Path
+from typing import Any, Dict, List, Union
 
 import pandas as pd
-
-from sklearn.model_selection import train_test_split
 
 # this scrip is write for dataset from kaggle:
 # https://www.kaggle.com/mrviswamitrakaushik/image-captioning-data
@@ -15,130 +14,102 @@ from sklearn.model_selection import train_test_split
 # https://www.kaggle.com/adityajn105/flickr30k
 
 
-def check_file_exist(path):
-    return os.path.exists(path)
+def check_exist(path: Union[str, Path]) -> bool:
+    """Check file exists."""
+    if isinstance(path, str):
+        path = Path(path)
+    return path.exists()
 
 
-def create_csv_coco(data: dict, directory: Dict[str, str]) -> pd.DataFrame:
+def create_dataset(data: Dict[str, Any], directories: List[Path]) -> pd.DataFrame:
     """
-    Function for create csv with 2 columns - path to image and text
-        description from coco dataset
-    :param data: json data about dataframe
-    :param directory: directories with images
-    :return: pandas.DataFrame
+    Create csv with 2 columns - path to image and text description.
+
+    Args:
+        data (Dict[str, Any]): json data about dataframe
+        directory (Dict[str, str]): directories with images
+
+    Returns:
+        pd.DataFrame: preprocessed coco dataset
     """
+    assert "images" in data, "Strange json"
     path_to_images = []
     text_descriptions = []
-    images = data['images']
-    print('Preprocess COCO dataset')
-    for image in images:
-        image_folder = image['filename'].split('_')[1]
-        text_description = [_['raw'] for _ in image['sentences']]
-        if image_folder == 'train2014':
-            path_to_image = path_join(directory['train'], image['filename'])
-        else:
-            path_to_image = path_join(directory['val'], image['filename'])
-        if not check_file_exist(path_to_image):
-            continue
-        path_to_images.extend([path_to_image] * len(text_description))
-        text_descriptions.extend(text_description)
-    return pd.DataFrame({'image': path_to_images, 'text': text_descriptions})
+    parts = []
+    print("Preprocess COCO dataset")
+    for image in data["images"]:
+        image_name = image["filename"]
+        text_description = [_["raw"] for _ in image["sentences"]]
+        part = image["split"]
+        path = None
+        for directory in directories:
+            if check_exist(directory / image_name):
+                path = directory / image_name
+                break
+        if path is not None:
+            path_to_images.extend([str(path)] * len(text_description))
+            parts.extend([part] * len(text_description))
+            text_descriptions.extend(text_description)
+    return pd.DataFrame(
+        {"image": path_to_images, "text": text_descriptions, "part": parts}
+    )
 
 
-def create_csv_flickr(data: dict, directory: str) -> pd.DataFrame:
-    """
-    Function for create csv with 2 columns - path to image and text
-        description from flickr dataset
-    :param data: json data about dataframe
-    :param directory: directory with images
-    :return: pandas.DataFrame
-    """
-    path_to_images = []
-    text_descriptions = []
-    images = data['images']
-    print('Preprocess Flickr dataset')
-    for image in images:
-        text_description = [_['raw'] for _ in image['sentences']]
-        path_to_image = path_join(directory, image['filename'])
-        if not check_file_exist(path_to_image):
-            continue
-        path_to_images.extend([path_to_image] * len(text_description))
-        text_descriptions.extend(text_description)
-    return pd.DataFrame({'image': path_to_images, 'text': text_descriptions})
+def open_json(json_path: Union[str, Path]) -> Dict[str, Any]:
+    assert check_exist(json_path), f"File {json_path} doesn't exists"
+    with open(json_path) as file:
+        data = json.load(file)
+    return data
 
 
-if __name__ == '__main__':
+def union_jsons(jsons: List[Dict[str, Any]]) -> Dict[str, Any]:
+    """Union many jsons into one."""
+    assert len(jsons) > 0, "Get one or more jsons"
+    copy_json = deepcopy(jsons[0])
+    for data in jsons[1:]:
+        copy_json["images"].append(data["images"])
+        copy_json["dataset"] += "/" + data["dataset"]
+    return copy_json
+
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dir_jsons', type=str, help='Directory with jsons')
+    parser.add_argument("--jsons", type=str, help="Directory with jsons")
     parser.add_argument(
-        '--coco_train', type=str, help='Directory with training images'
+        "--dirs", type=str, help="Directories with images separated whitespace"
     )
-    parser.add_argument(
-        '--coco_valid', type=str, help='Directory with valid images'
-    )
-    parser.add_argument(
-        '--flickr8k', type=str, help='Directory with images from flickr8k'
-    )
-    parser.add_argument(
-        '--flickr30k', type=str, help='Directory with images from flickr30k'
-    )
-    parser.add_argument(
-        '--target_csv', type=str, help='Directory for train and valid .csv'
-    )
+    parser.add_argument("--target", type=str, help="Directory for resulting .csv")
     args = parser.parse_args()
-    dataset_arguments = {}
-    for file in os.listdir(args.dir_jsons):
-        dataset_name = file.split('_')[1].split('.')[0]
-        file_json = path_join(args.dir_jsons, file)
-        if dataset_name == 'coco':
-            dataset_arguments['coco'] = {
-                'func': create_csv_coco, 'json': file_json,
-                'directory': {
-                    'train': args.coco_train, 'val': args.coco_valid
-                }
-            }
-        elif dataset_name == 'flickr8k':
-            dataset_arguments['flickr8k'] = {
-                'func': create_csv_flickr,
-                'json': file_json,
-                'directory': args.flickr8k
-            }
-        elif dataset_name == 'flickr30k':
-            dataset_arguments['flickr30k'] = {
-                'func': create_csv_flickr,
-                'json': file_json,
-                'directory': args.flickr30k
-            }
-    df = None
-    for key in dataset_arguments:
-        func = dataset_arguments[key]['func']
-        dirs = dataset_arguments[key]['directory']
-        with open(dataset_arguments[key]['json']) as f:
-            json_data = json.load(f)
-            if df is None:
-                df = func(json_data, dirs)
-            else:
-                df = pd.concat([
-                    df,
-                    func(json_data, dirs)
-                ], axis=0)
-    print(f'Overall count of pairs image-text before filter is {df.shape[0]}')
-    df = df.drop_duplicates(subset=['text'])
-    print(f'Overall count of pairs image-text after filter is {df.shape[0]}')
 
-    grouped = df.groupby(by='image', as_index=False).agg({'text': 'count'})
+    target_dir = Path(args.target)
+    assert not check_exist(target_dir) or target_dir.is_dir()
 
-    one_images = grouped['image'][grouped.text == 1]
+    # find labels and create dataset
+    jsons_dir = Path(args.jsons)
+    jsons = list(jsons_dir.glob("*.json"))
 
-    # train/valid split
-    train, valid = train_test_split(
-        df[~df.image.isin(one_images)], random_state=42, train_size=0.78, stratify=df[~df.image.isin(one_images)].image
-    )
-    train = pd.concat([
-        train,
-        df[df.image.isin(one_images)]
-    ])
-    print(f'Train samples is {train.shape[0]}')
-    print(f'Eval samples is {valid.shape[0]}')
-    train.to_csv(path_join(args.target_csv, 'train.csv'), index=False)
-    valid.to_csv(path_join(args.target_csv, 'valid.csv'), index=False)
+    images_dirs = [Path(d) for d in re.findall(r"[^\s]+", args.dirs) if check_exist(d)]
+
+    all_json = union_jsons([open_json(j) for j in jsons])
+
+    dataframe = create_dataset(all_json, images_dirs)
+    print(f"Overall count of pairs image-text after filter is {dataframe.shape[0]}")
+
+    # train/valid/test split
+    train_index = dataframe.part == "train"
+    valid_index = dataframe.part == "valid"
+    test_index = dataframe.part == "test"
+
+    train = dataframe[train_index]
+    valid = dataframe[valid_index]
+    test = dataframe[test_index]
+
+    print(f"Train size is {train.shape[0]}")
+    print(f"validation samples is {valid.shape[0]}")
+    print(f"Test samples is {valid.shape[0]}")
+
+    target_dir.mkdir(parents=True, exist_ok=True)
+
+    train.to_csv((target_dir / "train.csv"), index=False)
+    valid.to_csv((target_dir / "valid.csv"), index=False)
+    test.to_csv((target_dir / "test.csv"), index=False)
